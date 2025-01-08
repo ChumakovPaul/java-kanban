2f6eaafc0869c961ffb56b742220c5d308ee2599
Paul Chumakov <paul.chumakof@yandex.ru>
2025-01-08 18:43:09 +0300

    Исправить ошибки после проверки GitHub Actions

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Random;
 
 public class Main {
